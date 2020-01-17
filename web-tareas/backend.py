80f0b758671145540ab27094f9121bad8d3ca263
Hopeman22 <juantaban@gmail.com>
2020-01-17 20:40:52 +0100

    Flask test and first index html

--- web-tareas/backend.py
+++ web-tareas/backend.py
@@ -1,10 +1,23 @@
-form flask import Flask
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/");
+@app.route("/")
 def home():
-    return "Hello! this is the main page <h1>HELLO<h1>"
+    return render_template("index.html")
+
+# @app.route("/login", methods=["POST", "GET"])
+# def login():
+#     if request.method == "POST":
+#         user = request.form["nm"]
+#         return redirect(url_for("user", usr=user))
+#     else:
+#         return render_template("login.html")
+
+# @app.route("/<usr>")
+# def user(usr):
+#     return f"<h1>{usr}</h1>"
+
 
 if __name__ == "__main__":
-    app.run();
+    app.run(debug=True)
