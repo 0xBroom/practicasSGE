3d69365bd2878954dd4e53b84a9454e19cd0068c
JonRevo <50838368+JonRevo@users.noreply.github.com>
2020-01-31 18:20:04 +0100

    Base de datoos

--- TrabajoFlask/run.py
+++ TrabajoFlask/run.py
@@ -6,28 +6,29 @@ from flask import request
 from flask_sqlalchemy import SQLAlchemy
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_file = "sqlite:///{}".format(os.path.join(project_dir, "../BD/lista.db"))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "BD\lista.db"))
+print(database_file)
 
 app = Flask(__name__)
-app.config["SQL_ALCHEMY_DATABASE_URI"] = database_file
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
 db = SQLAlchemy(app)
 
 
-class Tareas(db.Model):
-    id = db.Column(db.Integer, unique =True, nullable=False, primary_key=True, sqlite_autoincrement=True)
-    tarea = db.Column(db.String(80), unique=False, nullable=False, primary_key=False)
+class Tarea(db.Model):
+    id = db.Column(db.Integer, unique =True, nullable=False, primary_key=True)
+    nombre = db.Column(db.String(80), unique=False, nullable=False, primary_key=False)
 
     def __repr__(self):
-        return "<Title: {}>".format(self.title)
+        return "<Nombre: {}>".format(self.nombre)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.form:
-        tareas = Tareas(tarea=request.form.get("tarea"))
-        db.session.add(tareas)
+        db.session.add(Tarea(nombre=request.form.get("tarea")))
         db.session.commit()
-    return render_template('home.html')
+    tareas = Tarea.query.all()
+    return render_template("home.html", tareas=tareas)
 if __name__ == '__main__':
     app.run(debug=True)
 
